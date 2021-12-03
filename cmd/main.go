27e5b65611e36134645b424b7e2d921d48f20c44
Renol P. H <reshifr@gmail.com>
2021-12-04 00:57:42 +0700

    fix: `reload` set to `0`

--- cmd/main.go
+++ cmd/main.go
@@ -12,9 +12,11 @@ func main() {
 		reload = 0
 	}
 	onFailed := func() {
-		if reload == 0 || reload == 31 {
+		if reload == 0 || reload >= 13 {
 			exec.Command("bash", "-c", "service tor reload").Run()
 			log.Println("TOR service reloading...")
+			reload = 0
+			return
 		}
 		reload++
 	}
