165680b0439b161ddea4e51d744cef44406bdb0b
Renol P. H <reshifr@gmail.com>
2021-12-03 20:10:38 +0700

    update: scraping with plan

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import scraper "reshifr/sc-scraper/pkg"
+import (
+	"log"
+	"os/exec"
+	scraper "reshifr/sc-scraper/pkg"
+)
 
 func main() {
-	scraper.Load()
+	reload := 0
+	onSucceed := func() {
+		reload = 0
+	}
+	onFailed := func() {
+		if reload == 0 || reload == 31 {
+			exec.Command("bash", "-c", "service tor reload").Run()
+			log.Println("TOR service reloading...")
+		}
+		reload++
+	}
+	scraper.Scrap(uint64(9223372036854775808), onSucceed, onFailed)
 }
